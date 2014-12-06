bfc1f8217eccc98e653617f244fbf8abd1788534
Yannick Germeau <yannick.g@globulebleu.com>
2014-12-06 19:37:08 +0100

    Add Module method and change Command prompt alter

--- DrupalModuleCreate.py
+++ DrupalModuleCreate.py
@@ -3,38 +3,102 @@ import os, shutil
 import pprint
 
 def Window():
-	return sublime.active_window()
+    return sublime.active_window()
 
 class DmcreateCommand(sublime_plugin.WindowCommand):
-	def run(self, paths = [], name = ""):
-		import functools
-		#Window().run_command('hide_panel');
-		Window().show_input_panel("Module Name:", name, functools.partial(self.on_done, paths), None, None)
+    def run(self, paths = [], name = ""):
+        import functools
+        #Window().run_command('hide_panel');
+        Window().show_input_panel("Module Name:", name, functools.partial(self.on_done, paths), None, None)
 
-	def on_done(self, paths, name):
-		pprint.pprint(name)
-		
-		if sublime.ok_cancel_dialog('Create '+name+'.install'):
-			pprint.pprint(paths)
-		else:
-			pprint.pprint('nope')
-		# for item in SideBarSelection(paths).getSelectedDirectoriesOrDirnames():
-		# 	item = SideBarItem(item.join(name), True)
-		# 	console.log(item.join(name));
-		# 	if item.exists():
-		# 		sublime.error_message("Unable to create module, folder or file exists.")
-		# 		self.run(paths, name)
-		# 		return
-		# 	else:
-		# 		item.create()
-		# 		if not item.exists():
-		# 			sublime.error_message("Unable to create folder:\n\n"+item.path())
-		# 			self.run(paths, name)
-		# 			return
-		self.refresh()
-	def refresh(self):
-		try:
-			sublime.set_timeout(lambda:sublime.active_window().run_command('refresh_folder_list'), 200);
-			sublime.set_timeout(lambda:sublime.active_window().run_command('refresh_folder_list'), 1300);
-		except:
-			pass
+    def on_done(self, paths, name): 
+        self.create_module(paths, name)      
+        self.confirm_install(paths,name)      
+     
+
+    def create_module(self, paths, name):
+        for item in paths:
+            item = DMModule(item, name)
+
+            if item.exists():
+                sublime.error_message("Unable to create file, file or folder exists.")
+                self.run(paths, name)
+                return
+            else:
+                try:
+                    item.create()
+                    item.edit()
+                except:
+                    sublime.error_message("Unable to create file:\n\n"+item.path())
+                    self.run(paths, name)
+                    return
+        self.refresh()
+
+    def confirm_install(self, paths, name):
+        import functools
+        Window().run_command('hide_panel');
+        yes = []
+        yes.append('Create '+name+'.install')
+        no = []
+        no.append('No')
+        
+        if sublime.platform() == 'osx':
+            sublime.set_timeout(lambda:Window().show_quick_panel([yes, no], functools.partial(self.on_confirm_install, paths)), 200)
+        else:
+            Window().show_quick_panel([yes, no], functools.partial(self.on_confirm_install, paths, name))
+
+    def on_confirm_install(self, paths,name, result):
+        pprint.pprint(paths)
+        pprint.pprint(name)
+        pprint.pprint(result)
+
+    def refresh(self):
+        try:
+            sublime.set_timeout(lambda:Window().run_command('refresh_folder_list'), 200);
+            sublime.set_timeout(lambda:Window().run_command('refresh_folder_list'), 1300);
+        except:
+            pass
+
+
+class DMModule:
+    def __init__(self, path, name):
+        self._path = path
+        self._name = name
+    
+
+    def exists(self):
+        return os.path.isdir(self.path()) or os.path.isfile(self.path())
+
+    def create(self):
+        self.dirnameCreate()
+        os.makedirs(self.path(), 0o775)
+    
+    def dirnameCreate(self):
+        try:
+            os.makedirs(self.dirname(), 0o775)
+        except:
+            pass
+
+    def dirname(self):
+        branch, leaf = os.path.split(self.path())
+        return branch;
+
+    def path(self, path = ''):
+        if path == '':
+            return self._path
+        else:
+            self._path = path
+            return path
+
+    def dirnameCreate(self):
+        try:
+            os.makedirs(self.dirname(), 0o775)
+        except:
+            pass
+
+    def name(self):
+        branch, leaf = os.path.split(self.path())
+        return leaf;
+
+    def join(self, name):
+        return os.path.join(self.path(), name)
